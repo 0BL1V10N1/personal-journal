0f84f324017a42a6dc4ffc67b341d05d0853cd5c
0BL1V10N <cvjeticaxel@gmail.com>
2026-02-10 19:04:57 +0100

    feat: add Kotlin serialization plugin and update theme names

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.3.10" apply false
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.8" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.10" apply false
 }
 
 subprojects {
