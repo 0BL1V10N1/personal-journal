e520acc604cd2e7ff32b8c4d37f445bfee88d650
0BL1V10N <cvjeticaxel@gmail.com>
2026-02-24 19:39:47 +0100

    feat: enable code minification for release build and update release workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             signingConfig = signingConfigs.getByName("release")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
