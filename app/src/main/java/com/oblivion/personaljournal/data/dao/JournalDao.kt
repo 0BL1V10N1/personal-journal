5db31aac5e0b1461f385ce05a0ad05991473d22e
0BL1V10N1 <cvjeticaxel@gmail.com>
2026-03-01 17:50:06 +0100

    refactor: remove wildcard import

--- app/src/main/java/com/oblivion/personaljournal/data/dao/JournalDao.kt
+++ app/src/main/java/com/oblivion/personaljournal/data/dao/JournalDao.kt
@@ -1,6 +1,11 @@
 package com.oblivion.personaljournal.data.dao
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
 import com.oblivion.personaljournal.data.entity.JournalEntity
 import kotlinx.coroutines.flow.Flow
 
